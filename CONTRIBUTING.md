675f625ea98272790fb793bb1655f4cfe817c0b2
Mario9810 <32363631+Mario9810@users.noreply.github.com>
2021-08-19 19:34:19 -0600

    client

---
+++ CONTRIBUTING.md
@@ -0,0 +1,1 @@
+This project is closed for contributions, but feel free to use the code to make other cool things. 
---
+++ constants.py
@@ -0,0 +1,34 @@
+"""
+    constants.py
+    Author: Pablo Ruiz 18259 (PingMaster99)
+    Version 1.0
+    Updated August 12, 2021
+
+    Constants used for XMPP client
+"""
+
+APP_MENU = """
+Select an option to proceed:
+1 -> Register a new account
+2 -> Login
+3 -> Exit
+>> """
+
+
+MAIN_MENU = """
+Select an option to proceed:
+1 -> Log out
+2 -> Delete account
+3 -> Show all users & contacts
+4 -> Add a user to contacts
+5 -> Start a conversation
+6 -> Join a group chat
+7 -> Define presence message
+8 -> Send a file
+9 -> Show contact details
+10 -> Exit
+>> """
+
+
+SERVER = "@alumchat.xyz"    # Change to @192.168.56.1 or ipv4 value if using a local server
+LOGGING = False             # Change to True if you want logging
